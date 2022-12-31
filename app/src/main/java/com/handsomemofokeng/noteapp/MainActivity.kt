acae70e4a50dce632e2a9c52d436ceda59de6cf0
Handsome Mofokeng <handsome.mofokeng@standardbank.co.za>
2022-12-31 19:45:37 +0200

    Initial commit

---
+++ app/src/main/java/com/handsomemofokeng/noteapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.handsomemofokeng.noteapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.handsomemofokeng.noteapp.ui.theme.NoteAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NoteAppTheme {
+
+            }
+        }
+    }
+}
