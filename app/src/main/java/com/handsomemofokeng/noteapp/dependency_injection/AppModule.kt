d894bb1431acf3e36f63c0eaa60015912c85cff1
Handsome Mofokeng <handsome.mofokeng@standardbank.co.za>
2023-01-03 08:58:56 +0200

    created AppModule.kt to provide database and repo

---
+++ app/src/main/java/com/handsomemofokeng/noteapp/dependency_injection/AppModule.kt
@@ -0,0 +1,45 @@
+package com.handsomemofokeng.noteapp.dependency_injection
+
+import android.app.Application
+import androidx.room.Room
+import com.handsomemofokeng.noteapp.feature_note.data.data_source.NoteDatabase
+import com.handsomemofokeng.noteapp.feature_note.data.repository.NoteRepository
+import com.handsomemofokeng.noteapp.feature_note.domain.repository.NoteRepositoryImpl
+import com.handsomemofokeng.noteapp.feature_note.domain.use_case.DeleteNote
+import com.handsomemofokeng.noteapp.feature_note.domain.use_case.GetNotes
+import com.handsomemofokeng.noteapp.feature_note.domain.use_case.NotesUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideNotesDatabase(app: Application): NoteDatabase {
+        return Room.databaseBuilder(
+            app,
+            NoteDatabase::class.java,
+            NoteDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideNotesRepository(db: NoteDatabase): NoteRepository {
+        return NoteRepositoryImpl(db.noteDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteUseCases(repository: NoteRepository): NotesUseCases {
+        return NotesUseCases(
+            getNotes = GetNotes(repository),
+            deleteNote = DeleteNote(repository)
+        )
+    }
+}
